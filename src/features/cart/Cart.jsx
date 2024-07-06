f8ae721fb51d431af3389ab6f0a617952bfccb47
DanielValik <danilvalik@gmail.com>
2024-07-06 18:29:03 +0200

    redux implemented + userSlice

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import Button from "../../ui/Button";
 import LinkButton from "../../ui/LinkButton";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -32,13 +33,19 @@ function Cart() {
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
         <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
-        <Button type="primary">Clear cart</Button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Daniel</div>;
+  const username = useSelector((state) => state.user.username);
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
